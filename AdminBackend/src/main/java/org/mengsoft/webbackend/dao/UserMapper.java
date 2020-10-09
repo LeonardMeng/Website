823d944ae5068cc7ff0f24c8819b742978ab5cd6
Lingjun Meng <lingjun.meng@student.unimelb.edu.au>
2020-10-10 00:43:55 +0800

    Remove Admin part

--- AdminBackend/src/main/java/org/mengsoft/webbackend/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package org.mengsoft.webbackend.dao;
-
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
-import org.mengsoft.webbackend.model.User;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserMapper extends Mapper<User> {
-}
